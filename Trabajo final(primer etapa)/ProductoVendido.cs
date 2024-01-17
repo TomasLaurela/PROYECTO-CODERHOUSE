c7c723b0a037e281ff44050f7e4c24d55c54f0d9
TomasLaurela <150181601+TomasLaurela@users.noreply.github.com>
2024-01-17 11:31:26 -0300

    Actualizar ProductoVendido.cs

--- Trabajo final(primer etapa)/ProductoVendido.cs
+++ Trabajo final(primer etapa)/ProductoVendido.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +8,26 @@ namespace Trabajo_final_primer_etapa_
 {
     public class ProductoVendido
     {
-        protected long id;
-        protected long IdProducto;
-        protected long Stock;
-        protected long IdVenta;
+        private int _id;
+        private int _idProducto;
+        private int _stock;
+        private int _idVenta;
 
+        public ProductoVendido() { }
+        public ProductoVendido(int id, int idProducto, int stock, int idVenta)
+        {
+            this._id = id;
+            this._idProducto = idProducto;
+            this._stock = stock;
+            this._idVenta = idVenta;
+           
+        }
+
+        public int Id { get { return _id; } set { _id = value; } }
+        public int IdProducto { get { return _idProducto; } set { _idProducto = value; } }
+
+        public int Stock { get { return _stock; } set { _stock = value; } }
+
+        public int IdVenta { get { return _idVenta; } set { _idVenta = value; } }
     }
 }
