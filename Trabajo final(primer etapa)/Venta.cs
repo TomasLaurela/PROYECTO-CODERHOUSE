e39c7c073b693bf17231fb03e440d738e80eed1a
TomasLaurela <150181601+TomasLaurela@users.noreply.github.com>
2024-01-17 13:06:38 -0300

    Actualizar Venta.cs

--- Trabajo final(primer etapa)/Venta.cs
+++ Trabajo final(primer etapa)/Venta.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +8,27 @@ namespace Trabajo_final_primer_etapa_
 {
    public class Venta
     {
-        protected long Id;
-        protected string Comentarios;
-        protected long IdUsuarios;
+        private int _id;
+        private string _comentarios;
+        private int _idUsuario;
+
+        public Venta(){
+        }
+
+        public Venta(int id, string comentarios, int idUsuario)
+        {
+            this._id = id;
+            this._comentarios = comentarios;
+            this._idUsuario = idUsuario;
+            
+        }
+
+        public int Id { get { return _id; } set { _id = value; } }
+
+        public string Comentarios { get { return _comentarios; } set { _comentarios = value; } }
+
+        public int IdUsuario { get { return _idUsuario; } set { _idUsuario = value; } }
     }
+}
+    
 }
