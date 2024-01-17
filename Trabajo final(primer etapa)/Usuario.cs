19197235eff121f74b9411459aa61773f73ca569
TomasLaurela <150181601+TomasLaurela@users.noreply.github.com>
2024-01-17 13:04:31 -0300

    Actualizar Usuario.cs

--- Trabajo final(primer etapa)/Usuario.cs
+++ Trabajo final(primer etapa)/Usuario.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +8,37 @@ namespace Trabajo_final_primer_etapa_
 {
     public class Usuario
     {
-        protected long Id;
-        protected string Nombre;
-        protected string Apellido;
-        protected string NombreUsuario;
-        protected long Contraseña;
-        protected string Mail;
+        private int _id;
+        private string _nombre;
+        private string _apellido;
+        private string _nombreUsuario;
+        private string _contrasena;
+        private string _mail;
 
+        public Usuario() { 
+        }
+        public Usuario(int id, string nombre, string apellido, string nombreUsuario, string contrasena, string mail)
+        {
+            this._id = id;
+            this._nombre = nombre;
+            this._apellido = apellido;
+            this._nombreUsuario = nombreUsuario;
+            this._contrasena = contrasena;
+            this._mail = mail;
+        }
+
+
+        public int Id { get { return _id; } set {_id = value; } }
+        public string Nombre { get { return _nombre; } set { _nombre = value; } }
+        public string Apellido { get { return _apellido; } set { _apellido = value; } }
+        public string NombreUsuario { get { return _nombreUsuario; } set { _nombreUsuario = value; } }
+        public string Contrasena { get { return _contrasena; } set { _contrasena = value; } }
+        public string Mail { get { return _mail; } set { _mail = value; } }
+
+    }
+
+    
+}
 
     }
 }
