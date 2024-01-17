673244ab147c1ba1a026d43beb19965ff8b1674f
TomasLaurela <150181601+TomasLaurela@users.noreply.github.com>
2024-01-17 18:22:13 -0300

    Actualizar Producto.cs

--- Trabajo final(primer etapa)/Producto.cs
+++ Trabajo final(primer etapa)/Producto.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +8,37 @@ namespace Trabajo_final_primer_etapa_
 {
     public class Producto
     {
-        protected long id;
-        protected string descripcion;
-        protected long Costo;
-        protected long PrecioVenta;
-        protected long Stock;
-        protected long IdUsuario;
+        private int _id;
+        private string _descripcion;
+        private double _costo;
+        private double _precioVenta;
+        private int _stock;
+        private int _idUsuario;
 
+        public Producto(int id, string descripcion, double costo,
+            double precioVenta, int stock, int idUsuario)
+        {
+            _id = id;
+            _descripcion = descripcion;
+            _costo = costo;
+            _precioVenta = precioVenta;
+            _stock = stock;
+            _idUsuario = idUsuario;
+        }
 
-    }
+        public int Id { get => _id; set => _id = value; }
+        public string Descripcion { get => _descripcion; set => _descripcion = value; }
+        public double Costo { get => _costo; set => _costo = value; }
+        public double PrecioVenta { get => _precioVenta; set => _precioVenta = value; }
+        public int Stock { get => _stock; set => _stock = value; }
+        public int IdUsuario { get => _idUsuario; set => _idUsuario = value; }
+
+        public string MostrarDatos()
+        {
+            StringBuilder sb = new();
+            sb.AppendLine($"Producto: {this.Descripcion} \n");
+            sb.AppendLine($"Stock: {this.Stock} \n");
+            sb.AppendLine($"Precio de Venta: {this.PrecioVenta} \n");
+            return sb.ToString();
+        }
 }
